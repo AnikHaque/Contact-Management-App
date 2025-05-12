7e82adccd6565ea090cc5e1fa6336d74fa62124b
AnikHaque <ekramul15-811@diu.edu.bd>
2025-05-12 14:11:14 +0600

    while loop added

--- contact_manager.php
+++ contact_manager.php
@@ -6,3 +6,12 @@ $contact2_name = "";
 $contact2_phone = "";
 
 $running = true;
+
+while ($running) {
+    echo "\n--- Contact Management App ---\n";
+    echo "1. Add Contact\n";
+    echo "2. View Contacts\n";
+    echo "3. Exit\n";
+    echo "Choose an option (1-3): ";
+    $choice = trim(fgets(STDIN));
+}
